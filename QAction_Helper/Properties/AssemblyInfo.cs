de3ed8074c03292f18364519984e5f2fcdd895ea
Pedro Almeida <pedro.almeida@skyline.be>
2021-12-02 15:38:42 +0000

    Updated Solution
    Change-Id: Ibd902138f66e00863e17dff7fb263ee244f2def1

---
+++ QAction_Helper/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QAction_Helper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("QAction_Helper")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0CB9096C-5A8D-4800-9D25-4336CD812456")]
+[assembly: AssemblyVersion("1.0.0.0")]
